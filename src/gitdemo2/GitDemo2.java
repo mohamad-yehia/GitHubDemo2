43db4caba1300f896c94bcdce6d1cb77b48bb0a5
mohamad yehia <mohamad.yeh@gmail.com>
2015-01-30 12:39:58 -0500

    modification number 1

--- src/gitdemo2/GitDemo2.java
+++ src/gitdemo2/GitDemo2.java
@@ -13,7 +13,7 @@ public class GitDemo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Test");
     }
     
 }
