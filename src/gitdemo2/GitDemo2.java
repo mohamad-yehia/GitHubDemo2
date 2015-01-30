40f3dfa092b3b01ef9d13e60d0ff12ef5c9efb9f
mohamad yehia <mohamad.yeh@gmail.com>
2015-01-30 01:15:21 -0500

    Ajouter le projet GitDemo2

---
+++ src/gitdemo2/GitDemo2.java
@@ -0,0 +1,19 @@
+/*
+ * copyright Mohamad Yehia.
+ */
+package gitdemo2;
+
+/**
+ *
+ * @author Mohamad
+ */
+public class GitDemo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
